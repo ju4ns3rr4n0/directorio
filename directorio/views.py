7216ea100f535ba999541d02dfe30b61e2265ded
ju4ns3rr4n0 <ju4ns3rr4n0@gmail.com>
2017-04-22 17:35:53 +0200

    modificada plantilla para que muestre datos de la BBDD

--- directorio/views.py
+++ directorio/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Directorio
 
 def directorio_list(request):
-    return render(request, 'directorio/directorio_list.html', {})
+    directorios = Directorio.objects.order_by('persona')
+    return render(request, 'directorio/directorio_list.html', {'directorios':directorios})
